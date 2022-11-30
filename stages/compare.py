44b0b227dddad9d2b718d101e1e8624e191657af
Yury <yury@iterative.ai>
2022-11-30 13:16:02 +0100

    cleanup

--- stages/compare.py
+++ stages/compare.py
@@ -1,27 +1,15 @@
 import argparse
 import sys
 from pathlib import Path
-import dvclive
-
-
 
 src_path = Path(__file__).parent.parent.resolve()
 sys.path.append(str(src_path))
-from mlem.api import load
 import gto
 import joblib
 import dvc.api
-import git
 from utils.load_params import load_params
 import json
-# repo = "."
-# # repo="https://github.com/ykasimov/demo-bank-customer-churn"
-# revision = gto.api.find_versions_in_stage(repo=repo, name="randomforest-model", stage="dev")[0].ref
-# with dvc.api.open("model/clf-model", repo=repo, rev=revision, mode="rb") as f:
-#     model = joblib.load(f)
-# print()
-# # gto.api.GitRegistry.
-# print(model)
+
 
 def compare(model_path: str, compare_to: str):
     new_model = joblib.load(model_path)
@@ -33,14 +21,13 @@ def compare(model_path: str, compare_to: str):
 
     ### your comparison code here
     best_model = new_model
-    # gto.api.register(git.Repo.refs)
-    # gto.api.assign()
     metrics = {"old_model_metrics": {"acc": 0.8, "f1": 0.83},
-                "new_model_metrics": {"acc": 0.86, "f1": 0.88}}
+               "new_model_metrics": {"acc": 0.86, "f1": 0.88}}
     with open("comparison_metrics.json", "w") as f:
         json.dump(metrics, f)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args_parser = argparse.ArgumentParser()
     args_parser.add_argument('--config', dest='config', required=True)
     args = args_parser.parse_args()
--- stages/train.py
+++ stages/train.py
@@ -26,27 +26,27 @@ def train(data_dir,
           model_type,
           cat_cols,
           random_state,
-          update_model:str=None,
+          update_model: str = None,
           **train_params):
-    X_train = pd.read_pickle(data_dir/'X_train.pkl')
-    y_train = pd.read_pickle(data_dir/'y_train.pkl')
+    X_train = pd.read_pickle(data_dir / 'X_train.pkl')
+    y_train = pd.read_pickle(data_dir / 'y_train.pkl')
 
     if model_type == "randomforest":
-        clf = RandomForestClassifier(random_state=random_state, 
-                                 **train_params)
+        clf = RandomForestClassifier(random_state=random_state,
+                                     **train_params)
     elif model_type == "lightgbm":
-        clf = LGBMClassifier(random_state=random_state, 
-                                    **train_params)
+        clf = LGBMClassifier(random_state=random_state,
+                             **train_params)
     elif model_type == "xgboost":
-        clf = XGBClassifier(random_state=random_state, 
-                                    **train_params)
+        clf = XGBClassifier(random_state=random_state,
+                            **train_params)
 
     numeric_transformer = Pipeline(
         steps=[
             ("imputer", SimpleImputer()),
             ("scaler", StandardScaler())
-            ]
-        )
+        ]
+    )
     categorical_transformer = OrdinalEncoder()
 
     preprocessor = ColumnTransformer(
@@ -64,8 +64,8 @@ def train(data_dir,
     else:
         model = Pipeline(
             steps=[("preprocessor", preprocessor), ("clf", clf)]
-            )
-    
+        )
+
     model.fit(X_train, y_train)
     save(
         model,
@@ -73,11 +73,12 @@ def train(data_dir,
         sample_data=X_train
     )
 
+
 if __name__ == '__main__':
     args_parser = argparse.ArgumentParser()
     args_parser.add_argument('--config', dest='config', required=True)
     args = args_parser.parse_args()
-    
+
     params = load_params(params_path=args.config)
     data_dir = Path(params.base.data_dir)
     model_dir = Path(params.base.model_dir)
